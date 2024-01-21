b9d552833eea57fef7f2f56b280eb7c8f990d352
drcgoncalves <d.goncalves@swordhealth.com>
2024-01-21 00:03:19 +0000

    updates app

--- epl_fantasy/app.py
+++ epl_fantasy/app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from main import get_standing
 st.set_page_config(page_title="EPL Fantasy Here-we-go")
 
-st.title("Standings Here we go")
+st.title("Standings *Here we go* Fantasy league")
 
 standing = get_standing()
 
 latest_processed_gameweek = standing.most_recent_gw_number
 
-st.markdown(f"### Latest gameweek processed: {latest_processed_gameweek}")
+st.markdown(f"#### Latest gameweek processed: **{latest_processed_gameweek}**")
