77dad8a1168c729bdcf5738e259bf3545d6837de
Diogo Gonçalves <45169529+diogorcg@users.noreply.github.com>
2024-01-24 19:09:27 +0000

    update gameweek 21

--- epl_fantasy/gameweeks.py
+++ epl_fantasy/gameweeks.py
@@ -8,20 +8,20 @@ class Gameweeks(ExtendedEnum):
         number=21,
         fixtures=[
             Fixture(
-                home_team=(Teams.VOLTA_CRIS.value, 19),
-                away_team=(Teams.PAULINHO_FC.value, 47),
+                home_team=(Teams.VOLTA_CRIS.value, 56),
+                away_team=(Teams.PAULINHO_FC.value, 71),
             ),
             Fixture(
-                home_team=(Teams.DRAFT_V3.value, 23),
-                away_team=(Teams.LIGA_SABSEG_FTW.value, 23),
+                home_team=(Teams.DRAFT_V3.value, 28),
+                away_team=(Teams.LIGA_SABSEG_FTW.value, 30),
             ),
             Fixture(
-                home_team=(Teams.AL_NASSR.value, 23),
-                away_team=(Teams.ZE_MAIA_GT_BINO.value, 28),
+                home_team=(Teams.AL_NASSR.value, 55),
+                away_team=(Teams.ZE_MAIA_GT_BINO.value, 37),
             ),
             Fixture(
-                home_team=(Teams.GYO_GT_HAALAND.value, 40),
-                away_team=(Teams.CHORA_BEBE.value, 46),
+                home_team=(Teams.GYO_GT_HAALAND.value, 51),
+                away_team=(Teams.CHORA_BEBE.value, 69),
             ),
         ],
     )
